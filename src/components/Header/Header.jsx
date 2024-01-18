18e4ce3df6a9a8fdfb10cd3ad64046d495129b87
itisameerkhan <itisameerkhan@gmail.com>
2024-01-18 15:38:50 +0530

    header added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import './Header.scss';
 import youtubeLogo from '../../assets/youtube-logo.svg';
 import upload from '../../assets/upload.svg';
+import burger from '../../assets/youtube-svgs/svg6.svg';
 import Bell from '../../assets/youtube-svgs/svg13.svg';
 import user from '../../assets/pfp-white.png';
 import search from '../../assets/youtube-svgs/svg10.svg';
@@ -10,7 +11,9 @@ const Header = () => {
   return (
     <div className="header">
         <div className="header-1">
-          <span className="material-symbols-outlined">menu</span>
+          <div className="burger-icon">
+            <img src={burger} alt="burger-menu" className='header-icon' />
+          </div>
           <img src={youtubeLogo} alt="youtube-logo" className='youtube-logo' />
         </div>
         <div className="header-2">
@@ -25,8 +28,15 @@ const Header = () => {
           </div>
         </div>
         <div className="header-3">
+          <div className="res-search">
+            <img src={search} alt="search" className='header-icon' />
+          </div>
+          <div className="header-3-img upload-icon">
           <img src={upload} alt="upload" className='header-icon' />
+          </div>
+          <div className="header-3-img">
           <img src={Bell} alt="bell" className='header-icon' />
+          </div>
           <img src={user} alt="" className='header-icon user-icon' />
         </div>
     </div>
