66f10ef4740bb1e37394ea20ef1cea19f131ee42
itisameerkhan <itisameerkhan@gmail.com>
2024-01-20 12:26:46 +0530

    WatchPage implemented ▶️

--- src/components/VideoContainer/VideoContainer.jsx
+++ src/components/VideoContainer/VideoContainer.jsx
@@ -4,6 +4,7 @@ import { YOUTUBE_VIDEOS_API } from '../../utils/constants';
 import VideoCard from '../VideoCard/VideoCard';
 import { useSelector } from 'react-redux';
 import Shimmer from '../Shimmer/Shimmer';
+import { Link } from 'react-router-dom';
 
 const VideoContainer = () => {
 
@@ -34,7 +35,9 @@ const VideoContainer = () => {
       style={{gridTemplateColumns: isMenuOpen ? 'repeat(auto-fill, minmax(350px, 1fr))' : 'repeat(auto-fill, minmax(300px, 1fr))'}} 
     >
         {videos.map((data) => (
-          <VideoCard key={data.id} info={data} />
+          <Link key={data.id} to={`/watch?v=${data.id}`} >
+            <VideoCard info={data} />
+          </Link>
         ))}
     </div>
   )
--- src/components/WatchPage/WatchPage.jsx
+++ src/components/WatchPage/WatchPage.jsx
@@ -1,9 +1,33 @@
+import { useEffect } from 'react';
 import './WatchPage.scss';
+import { useDispatch } from 'react-redux';
+import { closeMenu } from '../../Redux/appSlice';
+import { useParams, useSearchParams } from 'react-router-dom';
 
 const WatchPage = () => {
-  return (
-    <div className='watch'>WatchPage</div>
-  )
+
+    const dispatch = useDispatch();
+    const [searchParams] = useSearchParams();
+
+    console.log(searchParams.get("v"));
+
+    useEffect(() => {
+        dispatch(closeMenu());
+    },[]);
+
+    return (
+        <div className='watch'>
+            <iframe 
+                width="560" 
+                height="315" 
+                src={`https://www.youtube.com/embed/${searchParams.get("v")} `}
+                title="YouTube video player" 
+                frameBorder="0" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                allowFullScreen>
+            </iframe>
+        </div>
+    )
 }
 
 export default WatchPage;
